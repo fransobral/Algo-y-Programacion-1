32de6cd87270a12b19883c4ceafc864af521971c
Francisco Sobral <82195717+fransobral@users.noreply.github.com>
2022-07-11 19:33:17 -0300

    Update wpp.py

--- Python/wpp.py
+++ Python/wpp.py
@@ -1,5 +1,5 @@
 import pywhatkit as pw
 
 
-pw.sendwhatmsg("+5491151762407", "Suerte lindaaaa!!!!\nLa vas a romper todaaaa, yo sigo durmiendo pero te quiero y sos la mejor del mundo en matematica asi q te va a ir re bien.\nSos vos, ultimo esfuerzo.",8,6)
+pw.sendwhatmsg("+5491151767407", "hjjhh.\nhhh.",8,6)
 pw.sendwhatmsg("+5491173607662", "prueba",8,6)
